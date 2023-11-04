ad4906effdddf8b2ff152cd30418905e345339d7
Jakub Stelmasiak <stelmon3@gmail.com>
2023-11-04 12:30:11 +0100

    Add removing class active

--- js/script.js
+++ js/script.js
@@ -6,9 +6,13 @@
   */
   const titleClickHandler = function(event){
     console.log('Link was clicked!');
-    console.log(event)
+    console.log(event);
   /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
