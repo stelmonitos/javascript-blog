b6e0ece6e1165e014911cf3ef3e023cbf51d25cf
Jakub Stelmasiak <stelmon3@gmail.com>
2023-11-04 14:01:27 +0100

    Add remove active from all articles

--- js/script.js
+++ js/script.js
@@ -16,7 +16,11 @@
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.active');
 
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
