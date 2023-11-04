e94e43fb236d64812068cd820dd46597dc683ea0
Jakub Stelmasiak <stelmon3@gmail.com>
2023-11-04 14:42:23 +0100

    Add active to clickedElements

--- js/script.js
+++ js/script.js
@@ -3,25 +3,31 @@
     const links = document.querySelectorAll('.titles a');
     console.log('links:', links);
   }); 
-  */
-  const titleClickHandler = function(event){
-    console.log('Link was clicked!');
-    console.log(event);
-  /* remove class 'active' from all article links  */
+*/
+const titleClickHandler = function(event){
+  console.log('Link was clicked!');
+  const clickedElement = this;
+  
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-  /* add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
+  
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.active');
 
   for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
+    activeArticle.classList.remove('.active');
   }
-  /* get 'href' attribute from the clicked link */
+  /*get 'href' attribute from the clicked link */
+  
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
